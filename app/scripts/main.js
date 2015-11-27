921067434f59b33d2d84be07d5ae6f7f22960774
Jonathan Wen <jonathanwen95@gmail.com>
2015-11-26 22:49:20 -0800

    cleaned up code

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,44 +1,53 @@
-var key = '8c78771934d3245c13a7ca25966549fa';
-var secret = '17e2425c2d5ec462';
-var baseUrl = 'https://api.flickr.com/services/rest/?';
+var initialset = 20;
+
+var tumblr = {
+	key: '8c78771934d3245c13a7ca25966549fa',
+	secret: '17e2425c2d5ec462',
+	baseUrl: 'https://api.flickr.com/services/rest/?',
+
+	init: function() {
+		var method = 'flickr.interestingness.getList';
+
+		$.get(this.baseUrl, 
+			{
+				method: method,
+				api_key:this.key,
+				per_page:initialset,
+				format: 'json'	
+			}
+		).always(function(o) {
+			console.log(o.responseText);
+			tumblr.parseResponse(o.responseText);
+		});
+	},
+
+	parseResponse: function(data) {
+		var jsonData = data.substring(14, data.length-1);
+	 	var obj = JSON.parse(jsonData);
+		console.log(obj);
+		this.createNode(obj);
+	},
+
+	createNode: function(obj) {
+		var url = obj.photos.photo.map(this.createURL);
+		var targ = $('.content');
+		console.log(url);
+		url.forEach(function(elem) {
+			$(targ).append("<img src='" + elem + "'>");
+		});
+	},
+
+	createURL: function(elem) {
+		var url = 'https://farm' + elem.farm + '.staticflickr.com/'
+				+ elem.server + '/' + elem.id + '_' + elem.secret
+				+ '.jpg';
+		return url;
+	}
+};
+
+
 
 $(document).ready(function() {
-	init();
+	tumblr.init();
 });
 
-function init() {
-	var method = 'method=flickr.interestingness.getList';
-	var args = 'api_key=' + key;
-	var format = 'format=json';
-	var data = method + '&' + args + '&' + format;
-
-	var jqxhr = $.get(baseUrl, data, function(o) {
-		console.log(o);
-	})
-	.always(function(o) {
-		parseResponse(o.responseText);
-	});
-}
-
-function parseResponse (data) {
-	var jsonData = data.substring(14, data.length-1);
- 	var obj = JSON.parse(jsonData);
-	console.log(obj);
-	createNode(obj);
-}
-
-function createNode(obj) {
-	var url = obj.photos.photo.map(createURL);
-	var targ = $('.content');
-	console.log(url);
-	url.forEach(function(elem, i, arr) {
-		$(targ).append("<img src='" + elem + "'>");
-	});
-}
-
-function createURL(elem, i, arr) {
-	var url = 'https://farm' + elem.farm + '.staticflickr.com/'
-			+ elem.server + '/' + elem.id + '_' + elem.secret
-			+ '.jpg';
-	return url;
-}
